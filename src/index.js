8120f28ca41261df999d0965d9853b157af764c8
Rebecca Rose <campbell@oliversrealfood.com.au>
2019-08-29 15:58:21 +1000

    Created hello world which displays current date and time.

--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-const name = 'Rebecca Rose'
-const element =  <h1>Hello, {name}</h1>
+function tick() {
+    const element = (
+        <div>
+            <h1>Hello, World!</h1>
+            <h2>Today's date is {new Date().toLocaleDateString()}.</h2>
+            <h2>It Is {new Date().toLocaleTimeString()}.</h2>
+        </div>
+    );
+    ReactDOM.render(element, document.getElementById('root'));
+}
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+setInterval(tick, 1000);
